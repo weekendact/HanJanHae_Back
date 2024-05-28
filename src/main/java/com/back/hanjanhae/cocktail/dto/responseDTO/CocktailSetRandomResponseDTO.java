7e104b463d6c08e7130207bde35e0f8efc99083c
kkkkkasp0 <tmddk5678.54@gmail.com>
2024-05-28 13:43:14 +0900

    Fix: Add responseDTO
    - CocktailSetResponseDTO 생성
    - 랜덤 값을 dto에 넣어서 전달

---
+++ src/main/java/com/back/hanjanhae/cocktail/dto/responseDTO/CocktailSetRandomResponseDTO.java
@@ -0,0 +1,10 @@
+package com.back.hanjanhae.cocktail.dto.responseDTO;
+
+import lombok.Data;
+
+@Data
+public class CocktailSetRandomResponseDTO {
+
+    private String cocktailName;
+    private String cocktailPicture;
+}
--- src/main/java/com/back/hanjanhae/cocktail/service/CocktailSetRandomService.java
+++ src/main/java/com/back/hanjanhae/cocktail/service/CocktailSetRandomService.java
@@ -1,5 +1,6 @@
 package com.back.hanjanhae.cocktail.service;
 
+import com.back.hanjanhae.cocktail.dto.responseDTO.CocktailSetRandomResponseDTO;
 import com.back.hanjanhae.cocktail.model.entity.Cocktail;
 import com.back.hanjanhae.cocktail.model.repository.CocktailRandomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +14,13 @@ import java.util.stream.IntStream;
 
 @Service
 public class CocktailSetRandomService {
-    @Autowired
-    private CocktailRandomRepository cocktailRandomRepository;
+    private final CocktailRandomRepository cocktailRandomRepository;
 
-    public List<Cocktail> recommendRandomCocktails() {
+    public CocktailSetRandomService(CocktailRandomRepository cocktailRandomRepository) {
+        this.cocktailRandomRepository = cocktailRandomRepository;
+    }
+
+    public List<CocktailSetRandomResponseDTO> recommendRandomCocktails() {
         // 모든 칵테일을 조회
         List<Cocktail> listCocktails = cocktailRandomRepository.findAll();
 
@@ -26,8 +30,15 @@ public class CocktailSetRandomService {
                         .mapToObj(i -> listCocktails.get(new Random().nextInt(listCocktails.size())))
                         .collect(Collectors.toList());
 
+        List<CocktailSetRandomResponseDTO> cocktailSetRandomResponseDTOList = new ArrayList<>();
 
-        return randomCocktails;
+        for (Cocktail cocktail : randomCocktails) {
+            CocktailSetRandomResponseDTO cocktailSetRandomResponseDTO = new CocktailSetRandomResponseDTO();
+            cocktailSetRandomResponseDTO.setCocktailName(cocktail.getCocktailName());
+            cocktailSetRandomResponseDTO.setCocktailPicture(cocktail.getCocktailPicture());
+            cocktailSetRandomResponseDTOList.add(cocktailSetRandomResponseDTO);
+        }
+
+        return cocktailSetRandomResponseDTOList;
     }
-
 }
