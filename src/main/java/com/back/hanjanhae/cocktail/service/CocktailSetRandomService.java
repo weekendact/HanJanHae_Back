474b9113bd050d4c55f7197e73f7a214cf8c01ac
kkkkkasp0 <tmddk5678.54@gmail.com>
2024-05-28 14:09:49 +0900

    Fix: Fix RandomService
    - 서비스에 데이터가 반복되는 오류 수정

--- src/main/java/com/back/hanjanhae/cocktail/service/CocktailSetRandomService.java
+++ src/main/java/com/back/hanjanhae/cocktail/service/CocktailSetRandomService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -24,12 +25,17 @@ public class CocktailSetRandomService {
         // 모든 칵테일을 조회
         List<Cocktail> listCocktails = cocktailRandomRepository.findAll();
 
-        // 랜덤하게 5개의 칵테일 선택
-        List<Cocktail> randomCocktails =
-                IntStream.range(0, 7)
-                        .mapToObj(i -> listCocktails.get(new Random().nextInt(listCocktails.size())))
-                        .collect(Collectors.toList());
+        // 랜덤하게 7개의 칵테일 선택 (중복 제거) - 리스트를 무작위로 섞은 후 상위 7개 반환
+        Collections.shuffle(listCocktails);
+        List<Cocktail> randomCocktails = listCocktails.stream().limit(7).collect(Collectors.toList());
 
+//        // 랜덤하게 7개의 칵테일 선택 -> 데이터가 반복되는 오류 발생
+//        List<Cocktail> randomCocktails =
+//                IntStream.range(0, 7)
+//                        .mapToObj(i -> listCocktails.get(new Random().nextInt(listCocktails.size())))
+//                        .collect(Collectors.toList());
+
+        //DTO로 매핑
         List<CocktailSetRandomResponseDTO> cocktailSetRandomResponseDTOList = new ArrayList<>();
 
         for (Cocktail cocktail : randomCocktails) {
